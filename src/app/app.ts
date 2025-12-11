d8c913368653a262a5d03ef811e52a5e4ab5cb34
Ferran <ferran.garcia@institutvidreres.cat>
2025-12-11 18:04:18 +0100

    Exercici 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Arxiu} from '../arxiu/arxiu';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Arxiu],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
